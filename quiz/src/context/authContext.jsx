e476306185a00b68d304661c774d587e7ffe1649
Aniffour <as5828801@gmail.com>
2024-01-01 17:51:59 +0100

    refactory(login)

--- quiz/src/context/authContext.jsx
+++ quiz/src/context/authContext.jsx
@@ -1,25 +1,57 @@
 import { useContext  , createContext, useReducer} from "react";
 
+
+
+
+
+
+            // for (var [cUsername,cPasssowrd] of Object.entries(data)){
+            //     if ((cUsername === username) && (cPasssowrd === passowrd)){
+            //     return {...state , username:action.payload.username , isAuthenticated : true , error : null, tentative:0}
+            //     }else {
+            //         return {...state ,tentative:state.tentative+1, isAuthenticated:false, error :'username or password are invalid'}
+            //      } 
+            // }
+
+            // const fakeAuth = {
+            //     username :'amine',
+            //     password : 'amine123',
+            // }
 const authContext = createContext()
-const fakeAuth = {
-    username :'amine',
-    password : 'amine123',
-}
 const initialState = {
     username : null,
     isAuthenticated : false,
     error: null ,
     tentative :0
 }
-function reducer(state , action){
 
+const baseUrl = 'http://localhost:4444/users'
+async function fetchData(){
+        const res = await fetch(baseUrl)
+        const data = await res.json()
+        return  data
+}
+const data = await fetchData()
+
+
+function reducer(state , action){
+    
+    function checkAuthentication(username,password ){
+        for (var [,credentials ] of Object.entries(data)){
+                if ((username === credentials.username) && (password === credentials.passowrd)){
+                return true
+                }
+            }
+        return false
+    } 
     switch (action.type) {
         case 'auth/login':
-            if (action.payload.username === fakeAuth.username && action.payload.password === fakeAuth.password){
+            if (checkAuthentication(action.payload.username , action.payload.password)){
                 return {...state , username:action.payload.username , isAuthenticated : true , error : null, tentative:0}
-            }else {
+            }else{
                 return {...state ,tentative:state.tentative+1, isAuthenticated:false, error :'username or password are invalid'}
-            } 
+                 } 
+        
         case 'auth/logout':
             return {...initialState}
         case 'cleanErrors':
